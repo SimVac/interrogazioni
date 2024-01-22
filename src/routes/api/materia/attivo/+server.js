87bc0c8b2038780c057f250dea442be0cbc27f61
SimVac <103424187+SimVac@users.noreply.github.com>
2024-01-22 09:30:39 +0100

    Update +server.js

--- src/routes/api/materia/attivo/+server.js
+++ src/routes/api/materia/attivo/+server.js
@@ -1,7 +1,6 @@
 import { updateMateriaAttivo } from '$lib/dbQueries';
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    if (!auth) return new Response();
     let data = await request.json();
     await updateMateriaAttivo(data.attivo, data.id);
     return new Response();
